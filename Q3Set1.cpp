aaa82367d8e85f3942bef387c0e4fae4a8440a67
janasheenakbar <97395050+janasheenakbar@users.noreply.github.com>
2022-01-12 19:57:13 +0530

    Update Q3Set1.cpp

--- Q3Set1.cpp
+++ Q3Set1.cpp
@@ -1,30 +1,29 @@
-#include <iostream>
-using namespace std;
-int countsubarray(int array[], int n, int k)
-{
-int count = 0;
-int i, j, mul;
- for (i = 0; i < n; i++) {
-if (array[i] < k)
-count++;
-mul = array[i];
-for (j = i + 1; j < n; j++) {
+int numSubarray Product Less Thank(vector< int>& nums, int k) {
 
-mul = mul * array[j];
-if (mul < k)
- count++;
- else
- break;
-}
-}
-return count;
+if (k == 0) return 0;
+
+int cnt = 0;
+
+int pro = 1;
+
+for (int i = 0, j = 0
+
+; j < nums.size(); j++) { pro *= nums[j];
+
+while
+
+(i <= j && pro >= k) {
+
+pro / nums [i++];
+
 }
 
-int main()
-{
-  int array[] = { 1, 2, 3, 4 };
-  int k = 10;
-  int size = sizeof(array) / sizeof(array[0]);
-   int count = countsubarray(array, size, k);
-  cout << count << "\n";
+cnt +=ji + 1;
+
+-
+
+}
+
+return cnt;
+
 }
